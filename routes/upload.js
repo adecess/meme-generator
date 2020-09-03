28b138e7ba6827438c3e767ca4155ba87b5f5a8f
adecess <axel.saliniere@protonmail.com>
2020-09-03 17:49:57 -0400

    updating child dependencies

--- routes/upload.js
+++ routes/upload.js
@@ -1,21 +1,18 @@
-const express = require('express')
-const multer = require('multer')
-const sharp = require('sharp')
-const { ensureAuthMeme } = require('../middleware/auth')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
 // @ desc Upload page
 // @route GET /upload
 router.get('/', (req, res) => {
-    if (req.isAuthenticated()){
-        res.render('upload', {
-            layout: 'main'
-        })
-    } else {
-        res.render('upload', {
-            layout: 'login'
-        })
-    }   
-})
+  if (req.isAuthenticated()) {
+    res.render('upload', {
+      layout: 'main',
+    });
+  } else {
+    res.render('upload', {
+      layout: 'login',
+    });
+  }
+});
 
-module.exports = router
+module.exports = router;
